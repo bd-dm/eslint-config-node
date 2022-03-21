deb39441f69930021f44cbf688d55b150d3e112b
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-03-21 17:36:39 +0300

    Added ignore pattern _ to unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,6 @@ module.exports = {
 		"no-shadow": "off",
 		"@typescript-eslint/no-shadow": ["error"],
 		"no-unused-vars": "off",
-		"@typescript-eslint/no-unused-vars": ["error"],
+		"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
 	},
 };
